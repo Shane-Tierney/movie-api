73367b7059695eafc98c54ebc54445165cd11139
Shane Tierney <sptierney713@gmail.com>
2022-05-15 20:05:18 -0400

    forgot to save

--- controllers/movies.js
+++ controllers/movies.js
@@ -8,19 +8,13 @@ const isRunning = () => {
   console.log('server is up on http://localhost:1337') // eslint-disable-line no-console
 }
 
-// const getDirector = (request, response) => {
-//     const { director } = request.params
-
-//     const foundMovie = movies.filter((movie) => movie.directors.includes(director))
-
-//     return response.send(foundMovie)
-// }
 
 const getDirectorOrTitle = (request, response) => {
   const { directorOrTitle } = request.params
 
-  // eslint-disable-next-line max-len
+  
   const foundMovie = movies.filter((movie) => {
+    // eslint-disable-next-line max-len
     return movie.title.includes(directorOrTitle) || movie.directors.find((director) => director.toLowerCase().includes(directorOrTitle.toLowerCase())) })
 
   return response.send(foundMovie)
@@ -28,4 +22,4 @@ const getDirectorOrTitle = (request, response) => {
 
 
 
-module.exports = { getAllMovies, isRunning, getDirectorOrTitle }
+module.exports = { getAllMovies, isRunning, getDirectorOrTitle, createMovie }
